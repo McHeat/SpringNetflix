2a0b5744c6425f4d766b4f68ab259741004e20c8
wangjingyuan <wangjingyuan@bestvike.com>
2018-05-09 13:31:45 +0800

    搭建Netflix

--- eureka_client/src/main/java/com/mcheat/cloud/eureka/client/EurekaClientApplication.java
+++ eureka_client/src/main/java/com/mcheat/cloud/eureka/client/EurekaClientApplication.java
@@ -3,9 +3,13 @@ package com.mcheat.cloud.eureka.client;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
