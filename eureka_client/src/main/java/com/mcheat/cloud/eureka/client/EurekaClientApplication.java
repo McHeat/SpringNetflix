24a292ec1c46f6cc35386f06193993f9f88496dc
wangjingyuan <wangjingyuan@bestvike.com>
2018-05-30 12:51:36 +0800

    搭建Netflix

--- eureka_client/src/main/java/com/mcheat/cloud/eureka/client/EurekaClientApplication.java
+++ eureka_client/src/main/java/com/mcheat/cloud/eureka/client/EurekaClientApplication.java
@@ -2,6 +2,7 @@ package com.mcheat.cloud.eureka.client;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
--- eureka_client/src/main/java/com/mcheat/cloud/eureka/client/controller/BaseController.java
+++ eureka_client/src/main/java/com/mcheat/cloud/eureka/client/controller/BaseController.java
@@ -1,7 +1,10 @@
 package com.mcheat.cloud.eureka.client.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,9 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BaseController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String init() {
-        return "Hello, Eureka Client";
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String home(@RequestParam String name) {
+        return "hi " + name + ",i am from port:" + port;
     }
 
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
+    }
 }
